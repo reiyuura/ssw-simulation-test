78d9f4ed85bd2c1390b94260aa805dcbe8114056
Fachri Muhammad Bandang Haq <60908604+fachribandang@users.noreply.github.com>
2023-11-20 11:33:07 +0700

    init

---
+++ data/user.js
@@ -0,0 +1,131 @@
+const users = [
+    {
+      "username": "gizmo123",
+      "password": "P@ssw0rd1"
+    },
+    {
+      "username": "fluffy56",
+      "password": "SecretPwd!2"
+    },
+    {
+      "username": "rocketPower",
+      "password": "SecurePass789"
+    },
+    {
+      "username": "nebulaStar",
+      "password": "RandomPwd$34"
+    },
+    {
+      "username": "jazzSphinx",
+      "password": "Pass123word!"
+    },
+    {
+      "username": "galacticXplorer",
+      "password": "P@ssMeNow"
+    },
+    {
+      "username": "quasarGazer",
+      "password": "StrongPwd567"
+    },
+    {
+      "username": "zenithStorm",
+      "password": "Pwd!7890"
+    },
+    {
+      "username": "cosmicBreeze",
+      "password": "BreezyPwd12"
+    },
+    {
+      "username": "pixelVortex",
+      "password": "Vortex!Pass"
+    },
+    {
+      "username": "lunarNomad",
+      "password": "MoonPwd123"
+    },
+    {
+      "username": "stellarByte",
+      "password": "ByteMePwd"
+    },
+    {
+      "username": "neptuneEcho",
+      "password": "Echo123Pwd"
+    },
+    {
+      "username": "cipherDust",
+      "password": "DustyPwd@"
+    },
+    {
+      "username": "aquaPulse",
+      "password": "PulsePwd123"
+    },
+    {
+      "username": "quantumFlare",
+      "password": "Flare!Pwd"
+    },
+    {
+      "username": "novaWhisper",
+      "password": "WhisperPwd567"
+    },
+    {
+      "username": "cosmicOracle",
+      "password": "Oracle!123"
+    },
+    {
+      "username": "xenonPinnacle",
+      "password": "XenonPwd789"
+    },
+    {
+      "username": "rhythmQuasar",
+      "password": "Quasar!Pwd"
+    },
+    {
+      "username": "astralJigsaw",
+      "password": "JigsawPwd12"
+    },
+    {
+      "username": "cosmicMystic",
+      "password": "MysticPwd@"
+    },
+    {
+      "username": "solarZephyr",
+      "password": "Zephyr123Pwd"
+    },
+    {
+      "username": "infinityCipher",
+      "password": "Cipher!Pwd567"
+    },
+    {
+      "username": "pulseNebula",
+      "password": "NebulaPwd123"
+    },
+    {
+      "username": "stellarHarmony",
+      "password": "HarmonyPwd@"
+    },
+    {
+      "username": "lunarEcho",
+      "password": "Echo!Pwd567"
+    },
+    {
+      "username": "quasarSpectra",
+      "password": "SpectraPwd123"
+    },
+    {
+      "username": "gravityPulse",
+      "password": "Pulse!Pwd@"
+    },
+    {
+      "username": "galacticRhythm",
+      "password": "RhythmPwd567"
+    },
+    {
+      "username": "orbitalWhisper",
+      "password": "Whisper!Pwd123"
+    },
+    {
+      "username": "cosmicFlare",
+      "password": "FlarePwd@"
+    }
+  ];
+  
