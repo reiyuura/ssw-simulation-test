0490d12eed3646e84d30b88254fc6288faa67cc1
Reiyuura <88714200+reiyuura@users.noreply.github.com>
2024-02-10 21:41:04 +0700

    change admin

--- data/admin.js
+++ data/admin.js
@@ -1,9 +1,6 @@
 const admin =[
 {
-    username : "RizkiPrabowoSenseiOHM",
-    password : "MHOiesneSowobarPikziR"
-},{
-    username : "1",
-    password : "123"
+    username : "user",
+    password : "admin"
 }
 ]
